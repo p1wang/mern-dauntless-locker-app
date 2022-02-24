f049283f08185bdcce6814969c72da96fb4a720e
bltz <bltz@bltzs-Air.ht.home>
2022-02-23 19:36:58 -0500

    changed ports back to regular 3000 and 5000

--- server/index.js
+++ server/index.js
@@ -9,8 +9,9 @@ import userRoutes from "./routes/user.js";
 const app = express();
 dotenv.config();
 
+// for testing connecting
 app.get("/", (req, res) => {
-  res.send("Hello to dauntless locker");
+  res.send("Hello to dauntless locker app");
 });
 
 // Each app.use(middleware) is called every time a request is sent to the server.
@@ -21,7 +22,7 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.CONNECTION_URL, {

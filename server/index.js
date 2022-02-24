b908da189e3ed42a09a3e20fb755c65a3d4efdff
bltz <bltz@bltzs-Air.ht.home>
2022-02-23 19:55:18 -0500

    fixed the bug of mongodb not working after deploying to heroku due to port value in package.json in backend

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 mongoose
   .connect(process.env.CONNECTION_URL, {
